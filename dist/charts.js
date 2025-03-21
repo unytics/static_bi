9eaf9670dee7cd81d02d2f0c2c6a8bf90343ab01
paul.marcombes <paul.marcombes@nickel.eu>
2025-03-21 08:19:53 +0000

    Deployed bba25d1 with MkDocs version: 1.6.1

--- dist/charts.js
+++ dist/charts.js
@@ -20,52 +20,8 @@ function markdown2html(markdown_content) {
 }
 
 
-class DataTable extends HTMLElement {
 
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
-    this.userContent = this.textContent ? markdown2html(this.textContent) : '';
-    this.innerHTML = this.userContent + '\nINITIALIZING!';
-    document.addEventListener('data-loaded', async (event) => {
-      const data = await this.get_data(event.detail);
-      this.render(data);
-    });
-  }
-
-  async get_data(data_manager) {
-    const dimension = this.getAttribute('dimension');
-    const measure = this.getAttribute('measure');
-    const table = this.getAttribute('table');
-    const order_by = this.getAttribute('order_by');
-    const limit = this.getAttribute('limit');
-    const query = `
-      select *
-      from ${table}
-      limit ${limit}
-    `;
-    const data = await data_manager.query(query);
-    return data;
-  }
-
-  render(data) {
-    const tableHeader = Object.keys(data[0]).map(key => `<th>${key}</th>`).join('');
-    const tableRows = data.map(row => `<tr>${Object.values(row).map(value => `<td>${humanize(value)}</td>`).join('')}</tr>`).join('');
-    this.innerHTML = `
-      <table>
-        <thead><tr>${tableHeader}</tr></thead>
-        <tbody>${tableRows}</tbody>
-      </table>
-    `;
-  }
-
-}
-
-
-
-class Chart extends HTMLElement {
+class ChartElement extends HTMLElement {
 
   constructor() {
     super();
@@ -75,17 +31,119 @@ class Chart extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     this.userContent = this.textContent ? markdown2html(this.textContent) : '';
     this.shadowRoot.innerHTML = this.userContent + '\nINITIALIZING!';
-    document.addEventListener('data-loaded', async (event) => {
-      const data = await this.get_data(event.detail);
-      const chart_config = this.build_chart_config(data);
-      this.render(chart_config);
-    });
+    this.render();
+    const event_to_listen = this.getAttribute('table') ? `data-loaded:${this.getAttribute('table')}` : 'data-loaded';
+    console.log('EVENT TO LISTEN', event_to_listen);
+    document.addEventListener(event_to_listen, async (event) => {this.render();})
   }
 
-  async get_data(data_manager) {
+  async render() {
+    if (!this.is_data_manager_ready()) {
+      return;
+    }
+    const data = await this.get_data();
+    this.generate_html(data);
+  }
+
+  is_data_manager_ready() {
+    const table_name = this.getAttribute('table');
+    if (window.data_manager === undefined) {
+      return false;
+    }
+    if (window.data_manager.db_ready === false) {
+      return false;
+    }
+    if (!table_name) {
+      return true;
+    }
+    if (table_name in window.data_manager.tables) {
+      return true;
+    }
+    return false;
+  }
+
+}
+
+
+class TableChart extends ChartElement {
+
+  constructor() {
+    super();
+  }
+
+  async get_data() {
+    const table = this.getAttribute('table');
+    const dimensions = this.getAttribute('dimensions');
+    const measures = this.getAttribute('measures');
+    const order_by = this.getAttribute('order_by');
+    const limit = this.getAttribute('limit');
+    const query = `
+      select
+        ${dimensions ? dimensions + ',' : ''}
+        ${measures ? measures + ',' : ''}
+        ${(!dimensions && !measures) ? '*' : ''}
+      from ${table}
+      ${measures ? 'group by ' + dimensions : ''}
+      ${order_by ? 'order by ' + order_by : ''}
+      ${limit ? 'limit ' + limit : ''}
+    `;
+    const data = await window.data_manager.query(query);
+    return data;
+  }
+
+  generate_html(data) {
+    const tableHeader = Object.keys(data[0]).map(key => `<th>${key}</th>`).join('');
+    const tableRows = data.map(row => `<tr>${Object.values(row).map(value => `<td>${humanize(value)}</td>`).join('')}</tr>`).join('');
+    this.shadowRoot.innerHTML = `
+      <table>
+        <thead><tr>${tableHeader}</tr></thead>
+        <tbody>${tableRows}</tbody>
+      </table>
+    `;
+  }
+
+}
+
+class TableDescriptionChart extends TableChart {
+
+  constructor() {
+    super();
+  }
+
+  async get_data() {
+    const table = this.getAttribute('table');
+    const data = await window.data_manager.describe_table(table);
+    return data;
+  }
+
+}
+
+
+class TablesListChart extends TableChart {
+
+  constructor() {
+    super();
+  }
+
+  async get_data() {
+    const data = await window.data_manager.show_tables();
+    return data;
+  }
+
+}
+
+
+
+class Chart extends ChartElement {
+
+  constructor() {
+    super();
+  }
+
+  async get_data() {
+    const table = this.getAttribute('table');
     const dimension = this.getAttribute('dimension');
     const measure = this.getAttribute('measure');
-    const table = this.getAttribute('table');
     const order_by = this.getAttribute('order_by');
     const limit = this.getAttribute('limit');
     const query = `
@@ -97,12 +155,16 @@ class Chart extends HTMLElement {
       order by ${order_by}
       limit ${limit}
     `;
-    const data = await data_manager.query(query);
+    const data = await window.data_manager.query(query);
     return data;
   }
 
-  build_chart_config(data) {
-    return {
+  generate_html(data) {
+    if (this.chart) {
+      this.chart.destroy();
+      this.chart = undefined;
+    }
+    const chart_config = {
       type: this.chart_type,
       data: {
         labels: data.map(row => row.dimension),
@@ -119,14 +181,7 @@ class Chart extends HTMLElement {
           }
         }
       }
-    }
-  }
-
-  render(chart_config) {
-    if (this.chart) {
-      this.chart.destroy();
-      this.chart = undefined;
-    }
+    };
     this.shadowRoot.innerHTML = this.userContent + '<div><canvas id="chart"></canvas></div>';
     this.chartElement = this.shadowRoot.getElementById('chart');
     this.chart = new chartjs.Chart(this.chartElement, chart_config);
@@ -173,47 +228,43 @@ class PieChart extends Chart {
 
 }
 
-class BarChartGrid extends HTMLElement {
+class BarChartGrid extends ChartElement {
 
   constructor() {
     super();
   }
 
-  connectedCallback() {
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = 'INITIALIZING!';
-    document.addEventListener('data-loaded', async (event) => {
-      const data = await this.get_dimension_columns(event.detail);
-      const chart_config = this.build_chart_config(data);
-      this.render(chart_config);
-    });
+  async get_data() {
+    const table = this.getAttribute('table');
+    const dimensions = await window.data_manager.list_dimensions_columns(table);
+    return dimensions;
   }
 
-  async get_dimension_columns(data_manager) {
-    const dimension = this.getAttribute('dimension');
-    const measure = this.getAttribute('measure');
+  generate_html(dimensions) {
     const table = this.getAttribute('table');
+    const measure = this.getAttribute('measure');
     const order_by = this.getAttribute('order_by');
     const limit = this.getAttribute('limit');
-    const query = `
-      select
-        ${dimension} as dimension,
-        ${measure} as measure,
-      from ${table}
-      group by 1
-      order by ${order_by}
-      limit ${limit}
-    `;
-    const data = await data_manager.query(query);
-    return data;
+    this.shadowRoot.innerHTML = dimensions.map(dimension => `
+      <bar-chart
+        table="${table}"
+        dimension="${dimension}"
+        measure="${measure}"
+        order_by="${order_by}"
+        limit="${limit}"
+      >
+      </bar-chart>`
+    ).join('');
   }
-
 }
 
 
 
-customElements.define("data-table", DataTable);
+customElements.define("table-chart", TableChart);
+customElements.define("table-description-chart", TableDescriptionChart);
+customElements.define("tables-list-chart", TablesListChart);
 customElements.define("line-chart", LineChart);
 customElements.define("bar-chart", BarChart);
 customElements.define("doughnut-chart", DoughnutChart);
 customElements.define("pie-chart", PieChart);
+customElements.define("bar-chart-grid", BarChartGrid);
--- dist/data_manager.js
+++ dist/data_manager.js
@@ -1,6 +1,19 @@
 import * as duckdb from "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.28.1-dev106.0/+esm";
 
 
+function arrow_table2vectors(arrow_table) {
+  if (!arrow_table) {
+    console.warn("arrow_table is null or undefined.  Returning null.");
+    return null;
+  }
+  const vectors = {};
+  const columns = arrow_table.schema.fields;
+  for (const column of columns) {
+    vectors[column.name] = arrow_table.getChild(column.name).toArray();
+  }
+  return vectors;
+}
+
 class DataManager extends HTMLElement {
 
   constructor() {
@@ -8,17 +21,20 @@ class DataManager extends HTMLElement {
   }
 
   async connectedCallback() {
-    await this.init_duckdb();
-    await this.load_data();
-  }
-
-  async init_duckdb() {
-    if (window._db !== undefined) {
-      this.db = window._db;
+    if (window.data_manager !== undefined) {
       console.log('CACHE HIT!');
       return;
     }
     console.log('CACHE MISSED!');
+    window.data_manager = this;
+    this.db_ready = false;
+    this.tables = {};
+    await this.init_duckdb();
+    this.db_ready = true;
+    await this.load_data();
+  }
+
+  async init_duckdb() {
     const jsdelivr_bundles = duckdb.getJsDelivrBundles();
     const bundle = await duckdb.selectBundle(jsdelivr_bundles);
     const worker_url = URL.createObjectURL(
@@ -31,27 +47,70 @@ class DataManager extends HTMLElement {
     this.db = new duckdb.AsyncDuckDB(logger, worker);
     await this.db.instantiate(bundle.mainModule, bundle.pthreadWorker);
     URL.revokeObjectURL(worker_url);
-    window._db = this.db;
+  }
+
+  async query2arrow_table(query) {
+    const conn = await this.db.connect();
+    const arrow_table = await conn.query(query);
+    await conn.close();
+    return arrow_table;
   }
 
   async query(query) {
-    const conn = await this.db.connect();
-    const result = (await conn.query(query)).toArray().map((row) => row.toJSON());
-    await conn.close();
-    console.log(JSON.stringify(result, (key, value) =>
-        typeof value === 'bigint'
-            ? value.toString()
-            : value // return everything else unchanged
-    , 4))
+    const arrow_table = await this.query2arrow_table(query);
+    const result = arrow_table.toArray().map((row) => row.toJSON());
+    // console.log(JSON.stringify(result, (key, value) =>
+    //     typeof value === 'bigint'
+    //         ? value.toString()
+    //         : value // return everything else unchanged
+    // , 4))
     return result;
   }
 
+  async query2vectors(query) {
+    const arrow_table = await this.query2arrow_table(query);
+    const vectors = arrow_table2vectors(arrow_table);
+    console.log(vectors);
+    return vectors;
+  }
+
   async load_file(name, file_url) {
-    await this.query(`create table if not exists ${name} as from "${file_url}"`);
+    if (name in this.tables) {
+      return;
+    }
+    // await this.query(`create table ${name} as select * from "${file_url}"`);
+
+    const res = await fetch(file_url);
+    const buffer = await res.arrayBuffer();
+    const uint8_array = new Uint8Array(buffer);
+    await this.db.registerFileBuffer(`${name}.parquet`, uint8_array);
+    await this.query(`create view '${name}' as select * from parquet_scan('${name}.parquet')`);
+
+    this.tables[name] = {file: file_url};
+    console.log('EMITTED', `data-loaded:${name}`)
+    this.dispatchEvent(new CustomEvent(`data-loaded:${name}`, {
+      detail: this,
+      bubbles: true,
+      composed: true
+    }));
+  }
+
+  async show_tables() {
+    return await this.query(`show tables`);
   }
 
   async describe_table(name) {
     return await this.query(`describe table ${name}`);
+  }
+
+  async list_columns(table_name) {
+    const data = await this.describe_table(table_name);
+    return data.map(row => row.name);
+  }
+
+  async list_dimensions_columns(table_name) {
+    const data = await this.describe_table(table_name);
+    return data.filter(row => row.column_type === 'VARCHAR').map(row => row.column_name);
   }
 
   async load_data() {
@@ -82,8 +141,3 @@ class DataManagerTable extends HTMLElement {
 
 customElements.define("data-manager", DataManager);
 customElements.define("data-manager-table", DataManagerTable);
-
-
-document.addEventListener('DOMContentLoaded', function() {
-window.db = document.getElementsByTagName('data-manager')[0];
-});
